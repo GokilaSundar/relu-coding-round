b37e74d27ebd60ad32e87443bc2b71866eecd04c
GokilaSundar <gokithilaga267@gmail.com>
2025-04-24 18:34:01 +0530

    Implement artists

--- src/components/Artists/Artists.jsx
+++ src/components/Artists/Artists.jsx
@@ -1,5 +1,24 @@
+import { Avatar } from "../Avatar/Avatar";
 import "./Artists.css";
+import { DummyArtists } from "./DummyArtists";
+
+const Artist = (props) => {
+  return (
+    <div className="artist">
+      <img alt={props.item.user.name} src={props.item.image} />
+      <Avatar user={props.item.user} online />
+    </div>
+  );
+};
 
 export const Artists = () => {
-  return <div className="">Artists</div>;
+  return (
+    <div className="artists-container">
+      <div className="artists">
+        {DummyArtists.map((item, index) => (
+          <Artist key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
---
+++ src/components/Artists/DummyArtists.jsx
@@ -0,0 +1,53 @@
+import Artist1 from "../../assets/artists/artist-1.png";
+import Artist2 from "../../assets/artists/artist-2.png";
+import Artist3 from "../../assets/artists/artist-3.png";
+import Artist4 from "../../assets/artists/artist-4.png";
+import Artist5 from "../../assets/artists/artist-5.png";
+import Image1 from "../../assets/artists/image-1.png";
+import Image2 from "../../assets/artists/image-2.png";
+import Image3 from "../../assets/artists/image-3.png";
+import Image4 from "../../assets/artists/image-4.png";
+import Image5 from "../../assets/artists/image-5.png";
+
+export const DummyArtists = [
+  {
+    image: Image1,
+    user: {
+      name: "Thomas Edward",
+      handle: "thewildwithyou",
+      avatar: Artist1,
+    },
+  },
+  {
+    image: Image2,
+    user: {
+      name: "Chris Doe",
+      handle: "thewildwithyou",
+      avatar: Artist2,
+    },
+  },
+  {
+    image: Image3,
+    user: {
+      name: "Emilie Jones",
+      handle: "thewildwithyou",
+      avatar: Artist3,
+    },
+  },
+  {
+    image: Image4,
+    user: {
+      name: "Jessica Williams",
+      handle: "thewildwithyou",
+      avatar: Artist4,
+    },
+  },
+  {
+    image: Image5,
+    user: {
+      name: "Thomas J",
+      handle: "thewildwithyou",
+      avatar: Artist3,
+    },
+  },
+];
