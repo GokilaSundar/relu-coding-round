c1f4db049d58659d15b584050d2d55669d9a219a
GokilaSundar <gokithilaga267@gmail.com>
2025-04-24 17:48:18 +0530

    Implement carousel using embla-carousel-react

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,46 @@
 import "./Carousel.css";
 
+import useEmblaCarousel from "embla-carousel-react";
+
+import Star from "../../assets/icons/star.svg?react";
+import { DummyCarousel } from "./DummyCarousel";
+
+const options = { slidesToScroll: 1, loop: true };
+
 export const Carousel = () => {
-  return <div className="">Carousel</div>;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  return (
+    <div className="carousel">
+      <div className="carousel-viewport" ref={emblaRef}>
+        <div className="carousel-container">
+          {DummyCarousel.map((item, index) => (
+            <div className="carousel-slide-container" key={index}>
+              <div className="carousel-slide">
+                <img alt={item.title} src={item.image} />
+                <div className="title">{item.title}</div>
+                <div className="price-container">
+                  <span className="price">${item.price}</span>
+                  <span className="ratings">
+                    {new Array(5).fill("").map((_, index) => (
+                      <Star
+                        key={index}
+                        style={
+                          index < item.rating
+                            ? {}
+                            : {
+                                filter: "grayscale(1)",
+                              }
+                        }
+                      />
+                    ))}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/Carousel/DummyCarousel.jsx
@@ -0,0 +1,31 @@
+import Image1 from "../../assets/carousel/image-1.png";
+import Image2 from "../../assets/carousel/image-2.png";
+import Image3 from "../../assets/carousel/image-3.png";
+import Image4 from "../../assets/carousel/image-4.png";
+
+export const DummyCarousel = [
+  {
+    image: Image1,
+    title: "Modern Wall Decor Framed Painting",
+    price: 199.99,
+    rating: 4,
+  },
+  {
+    image: Image2,
+    title: "Modern Wall Decor Framed Painting",
+    price: 199.99,
+    rating: 5,
+  },
+  {
+    image: Image3,
+    title: "Modern Wall Decor Framed Painting",
+    price: 199.99,
+    rating: 5,
+  },
+  {
+    image: Image4,
+    title: "Modern Wall Decor Framed Painting",
+    price: 199.99,
+    rating: 5,
+  },
+];
