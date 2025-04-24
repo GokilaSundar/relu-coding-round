f757c7461b97e02c1372ffe98f7c06f1a6d1957d
GokilaSundar <gokithilaga267@gmail.com>
2025-04-24 13:51:05 +0530

    Implement filter bar

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,5 +1,16 @@
 import "./FilterBar.css";
 
+import SearchIcon from "../../assets/icons/search.svg?react";
+import FilterIcon from "../../assets/icons/Filter.svg?react";
+
 export const FilterBar = () => {
-  return <div className="">FilterBar</div>;
+  return (
+    <div className="card filter-bar">
+      <SearchIcon />
+      <input placeholder="Search here..." />
+
+      <FilterIcon />
+      <div>Filters</div>
+    </div>
+  );
 };
