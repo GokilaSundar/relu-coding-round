0f434f35058c63e7f81121f2e90ddbb1da1ba712
GokilaSundar <gokithilaga267@gmail.com>
2025-04-24 10:22:31 +0530

    Setup react router with dummy routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<div>Login</div>} />
+      <Route path="/register" element={<div>Register</div>} />
+
+      <Route path="/dashboard" element={<div>Dashboard</div>} />
+
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
+  );
 };
 
 export default App;
